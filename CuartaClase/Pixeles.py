6718c3d3d628364a3175ddbfea9b48b6b2e6ceac
Ramiro Garcia <yosoysaban@gmail.com>
2023-04-18 17:47:11 -0600

    Pixeles

--- CuartaClase/Pixeles.py
+++ CuartaClase/Pixeles.py
@@ -1,8 +1,11 @@
-import cv2
-import numpy as np
+import cv2   # Importamos la biblioteca OpenCV
+import numpy as np   # Importamos la biblioteca NumPy
 
+# Creamos una matriz tridimensional de 100x100 píxeles con 3 canales de color (RGB) con valores de píxel inicializados en cero
 ImagenObscura = np.zeros((100, 100, 3), np.uint8)
 
-pixel = ImagenObscura[97,97]
+# Seleccionamos el valor de píxel en la posición [97,97] de la matriz ImagenObscura
+pixel = ImagenObscura[97, 97]
 
+# Imprimimos el valor del píxel seleccionado
 print(pixel)
