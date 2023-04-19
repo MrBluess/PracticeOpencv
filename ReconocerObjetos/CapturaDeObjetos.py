31108f9cfbd384e395a12a21a6f3721dab9199b1
Ramiro Garcia <yosoysaban@gmail.com>
2023-04-18 19:13:10 -0600

    Script para reconocer objetos

---
+++ ReconocerObjetos/CapturaDeObjetos.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+import imutils
+import os
+
+Datos = 'p'
+if not os.path.exists(Datos):
+    print('Carpeta creada: ', Datos)
+    os.makedirs(Datos)
+
+# Para capturar los datos vamos a estar utilizando un video streaming
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+x1, y1 = 190, 80
+x2, y2 = 450, 398
+
+count = 0
+
+while True:
+    ret, frame = cap.read()
+    if ret == False: break
+
+    cv2.imshow('frame', frame)
+    
+    k = cv2.waitKey(1)
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
